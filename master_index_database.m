88e3b364ef51e4651a57423ca249437077d4996c
Aaron Thode <athode@ucsd.edu>
2025-10-27 11:25:13 -0700

    REvised master_index_database and added estimate_airgun_interval.m, in prep for making customized unsupervised databases.

--- master_index_database.m
+++ master_index_database.m
@@ -16,8 +16,9 @@ day_want={'0815','0821','0829','0905','0913','0927'};
 folder_names={'Event_sounds.dir','Manually_selected_bowhead_calls.dir'};
 SNR_gram_dims=[128 104];
 
-debug.plot=true;
+param.interval_remove.debug=false;
 
+DASARs_counted=false;
 for Iyear=1:length(year_want)
     for Isite=1:length(Site)
         for Iday=1:length(day_want)
@@ -44,9 +45,8 @@ for Iyear=1:length(year_want)
                     for Ifile=1:Nfiles
                         temp=load([fnames(Ifile).folder filesep fnames(Ifile).name]);
                         if ~all(SNR_gram_dims==size(temp.SNR_gram))
-                            disp([fnames(Ifile).folder filesep fnames(Ifile).name 'SNR_gram not correct size:' size(temp.SNR_gram)]);
-                            eval(sprintf('!mv %s %s/Trash.dir',fnames(Ifile).name,database_folder));
-                            pause
+                            disp([fnames(Ifile).folder filesep fnames(Ifile).name 'SNR_gram not correct size:\n' size(temp.SNR_gram)]);
+                            eval(sprintf('!mv %s/%s %s/Trash.dir',fnames(Ifile).folder,fnames(Ifile).name,database_folder));
                             continue
                         end
                         index{Iyear,Isite,Iday,Ifold}.fname(Ifile,:)=fnames(Ifile).name;
@@ -59,135 +59,67 @@ for Iyear=1:length(year_want)
                     index{Iyear,Isite,Iday,Ifold}.bearing=index{Iyear,Isite,Iday,Ifold}.bearing(Igood);
                     index{Iyear,Isite,Iday,Ifold}.tabs=index{Iyear,Isite,Iday,Ifold}.tabs(Igood);
                     index{Iyear,Isite,Iday,Ifold}.is_airgun=zeros(1,length(Igood));
-
-                    %%%Optional search for airgun signals
-                    tabs=index{Iyear,Isite,Iday,Ifold}.tabs;
-                    tsec=(tabs-tabs(1))*24*3600;
-                    bearing=index{Iyear,Isite,Iday,Ifold}.bearing;
+                   
                     %%%Identify DASARs present
                     DASAR_ID=char(unique(index{Iyear,Isite,Iday,Ifold}.fname(:,5)));
-                    for Id=1:length(DASAR_ID)
-                        Iltr=find(double(DASAR_ID(Id))==index{Iyear,Isite,Iday,Ifold}.fname(:,5));
+                    if ~DASARs_counted
+                        DASARs_counted=true;
 
-                        if debug.plot & strcmpi(folder_names{Ifold},'Event_sounds.dir')
-                            figure(1);
-                            subplot(length(DASAR_ID),1,Id)
-                            plot(index{Iyear,Isite,Iday,Ifold}.tabs(Iltr),index{Iyear,Isite,Iday,Ifold}.bearing(Iltr),'.')
-                            xtickangle(90);grid on
-                            datetick('x',30)
-                        end
+                        file_fraction.manual_all=zeros(length(year_want),length(Site),length(day_want),length(DASAR_ID),4);  %Last index is maximum number of 'D' folders expected.
+                        file_fraction.auto_all=file_fraction.manual_all;
 
-                        %%Compute ICI (regular intervals) from raw data, using selected features
-                        %%for assistance.
-                       
-                        param.interval_remove.ICItol=[0.5 1.00];  %How close does a candidate have to be to predicted ICI time? (sec)
-                        param.interval_remove.ICI_range=[7 10;
-                            10 42];
-                        param.interval_remove.Ndet=20;
-                        param.interval_remove.Nmiss=12;
-                        param.interval_remove.tol_feature=15;
+                        file_fraction.manual_noairgun=zeros(length(year_want),length(Site),length(day_want),length(DASAR_ID),4);  %Last index is maximum number of 'D' folders expected.
+                        file_fraction.auto_noairgun=file_fraction.manual_noairgun;
 
-                        param.interval_remove.ICI_std=0.4;  %How close do adjacent ICI estimates have to be (sec)
-                        param.interval_remove.Nstd=7;      % How many ICIs must pass the 'ICI_std' test?
+                    end
 
-                        try
-                            ICI=compute_ici_bothways_feature(tabs(Iltr),param.interval_remove.ICI_range, ...
-                                bearing(Iltr), {'bearing'},...
-                                param.interval_remove.Ndet,param.interval_remove.Nmiss,...
-                                param.interval_remove.tol_feature,param.interval_remove.ICItol);
+                     %%%Record number of files present here
+                     %folder_names={'Event_sounds.dir','Manually_selected_bowhead_calls.dir'};
+                     for Id=1:length(DASAR_ID)
+                         Nfiles_DASAR=sum(double(DASAR_ID(Id))==index{Iyear,Isite,Iday,Ifold}.fname(:,5));
+                         if Ifold==1  %auto
+                             file_fraction.auto_all(Iyear,Isite,Iday,Id,Idd)=Nfiles_DASAR;
+                         else  %manual calls
+                             file_fraction.manual_all(Iyear,Isite,Iday,Id,Idd)=Nfiles_DASAR;
+                         end
+                     end
 
 
-                            %function [ICI]=compute_ici_bothways_feature(tabs,ici_range,feature_array,feature_names, ...
-                            %       num_clicks,num_misses,tol_feature,tol_time,Idebug),
-                            %
-                            %%% Given a vector of datenumbers, search for regular occurrances and
-                            %%% estimate inter-click interval (ICI) and multipath
-                            %    tabs: [1 x Ntimes] vector of datenumbers
-                            %       to enable the first detected sound to be assigned an ICI
-                            %    ici_range=[0.1 2; 5 10]; %Range of ICIs to test for in seconds.  If
-                            %       multiple rows, test each range
-                            %    feature_array: [Nfeature x Ntimes] array of features
-                            %    feature_names: cell array of feature names, corresponds with rows of
-                            %       feature array.
-                            %    num_clicks=3;  %number of adjacent detections to examine for ICI
-                            %    num_misses=1;  %How many "gaps" can exist in the ICI trace
-                            %    tol_feature: [Nfeature x 1] array of tolerances (absolute) for feature
-                            %       matching
-                            %    tol_time=[0.01; 0.5];  %Tolerance for accepting a detection if close to ICI,(sec)
-                            %       The number of rows much match the number of rows in ici_range.
-                            %    Idebug:
-                            %       .fname: raw GSI file from which data extracted.
-                            %       .names:  cell array (Nfeature cells) with descriptive string of
-                            %           features in feature_array.
-                            %       .index:  index of element of tabs to begin plotting debug output.
-                            %
-                        catch
-                            disp('Check your MATLAB path: ICI_detectors may be missing');
-                            keyboard
-                        end
+                     %%%Optional search for airgun signals
+                     param.interval_remove.ICItol=[0.5 1.00];  %How close does a candidate have to be to predicted ICI time? (sec)
+                     param.interval_remove.ICI_range=[7 10;
+                         10 42];
+                     param.interval_remove.Ndet=20;
+                     param.interval_remove.Nmiss=12;
+                     param.interval_remove.tol_feature=15;
 
-                        if debug.plot & strcmpi(folder_names{Ifold},'Event_sounds.dir')
-                            figure(2+10*Iday+Id)
+                     param.interval_remove.ICI_std=0.4;  %How close do adjacent ICI estimates have to be (sec)
+                     param.interval_remove.Nstd=7;      % How many ICIs must pass the 'ICI_std' test?
 
-                            subplot(2,2,1)
-                            plot(tabs(Iltr),ICI,'x');
-                            xtickangle(90);grid on
-                            datetick('x',30)
-                            title(fnames(Iltr(1)).name,'interp','none')
+                     for Id=1:length(DASAR_ID)
+                         Iltr=find(double(DASAR_ID(Id))==index{Iyear,Isite,Iday,Ifold}.fname(:,5));
+                         param.interval_remove.titstr=fnames(Iltr(1)).name;
+                         ICI=estimate_airgun_interval(index{Iyear,Isite,Iday,Ifold}.tabs(Iltr),index{Iyear,Isite,Iday,Ifold}.bearing(Iltr),param.interval_remove);
+                         index{Iyear,Isite,Iday,Ifold}.is_airgun(Iltr)=ICI;
 
-                            subplot(2,2,3)
-                            plot(bearing(Iltr),ICI,'x');grid on;xlabel('Azimuth (deg)');ylabel('ICI (sec)');
+                         Nfiles_DASAR=sum(ICI<1);
+                         if Ifold==1  %auto
+                             file_fraction.auto_noairgun(Iyear,Isite,Iday,Id,Idd)=Nfiles_DASAR;
+                         else  %manual calls
+                             file_fraction.manual_noairgun(Iyear,Isite,Iday,Id,Idd)=Nfiles_DASAR;
+                         end
+                     end %Id
 
-                        end
-                        %%Added April 5, 2010:  Sometimes walrus sequences and heavy bowhead whale
-                        %%sequences can have an ICI, but the ICI is inconsistent between calls.
-                        %  Thus here we march through each ICI detection and check whether detections
-                        %  nearby share the same ICI.
+                end %Idd
 
-                        Iguns=find(ICI>0);
+            end %Ifold
+        end %Iday
+    end %Isite
+end %Iyear
 
-                        ICI_score=ones(size(ICI));
-                        for I=1:length(Iguns)
-                            current_time=tsec(Iltr(Iguns(I)));
-                            current_ICI=ICI(Iguns(I));
-                            Itest=find(abs(current_time-tsec(Iltr(Iguns)))<=0.5*param.interval_remove.Ndet*current_ICI);
-                            Ipass=0;
-                            for J=1:2  %Harmonic loop:  checks for possibility that a 10 s ICI may have been assigned a 20 s ICI.
-                                Ipass=Ipass+length(find(abs(ICI(Iguns(Itest))/J-current_ICI)<=param.interval_remove.ICI_std));
-                            end
+file_fraction.auto_all=file_fraction.auto_all/sum(file_fraction.auto_all(:));
+file_fraction.manual_all=file_fraction.manual_all/sum(file_fraction.manual_all(:));
+file_fraction.manual_noairgun=file_fraction.manual_noairgun/sum(file_fraction.manual_noairgun(:));
+file_fraction.auto_noairgun=file_fraction.auto_noairgun/sum(file_fraction.auto_noairgun(:));
 
-                            %%Are there enough matching ICIs close to the value of the current ICI?
-                            if (Ipass-1)<param.interval_remove.Nstd
-                                ICI_score(Iguns(I))=0;
-                            else
-                                %disp('good');
-                            end
-
-                        end
-
-                        ICI=ICI.*ICI_score;
-
-                        if debug.plot & strcmpi(folder_names{Ifold},'Event_sounds.dir')
-                            %figure(2)
-                            subplot(2,2,2)
-                            plot(tabs(Iltr(ICI>0)),ICI(ICI>0),'x');
-                            xtickangle(90);grid on
-                            datetick('x',30)
-                            title(fnames(Iltr(1)).name,'interp','none')
-
-                            subplot(2,2,4)
-                            plot(bearing(Iltr),ICI,'x');grid on;xlabel('Azimuth (deg)');ylabel('ICI (sec)');
-                            pause
-                        end
-
-                        index{Iyear,Isite,Iday,Ifold}.is_airgun(Iltr)=ICI;
-                    end %Id
-
-                end
-
-            end
-        end
-    end
-end
-
-save([database_folder '/Database_index.mat'],'index','year_want','Site','day_want','folder_names','SNR_gram_dims');
+save([database_folder '/Database_index.mat'],'index','file_fraction','year_want','Site','day_want','folder_names','SNR_gram_dims');
