c36226e457617295621b9d52db606310190da69f
Aaron Thode <athode@ucsd.edu>
2026-06-15 12:06:27 -0700

    First working detector demo!

--- matlab/NeuralNetworkImports/load_trained_network.m
+++ matlab/NeuralNetworkImports/load_trained_network.m
@@ -1,4 +1,4 @@
-function [net_autoencoder, net_decoder,net_dir]=load_trained_network(keyword)
+function [net_autoencoder, net_decoder,net_dir,Nlatent]=load_trained_network(keyword)
 
 addpath ..
 [Database_dir,procdata_basedir,gitpath] = setUpDatabasePaths;
@@ -12,12 +12,13 @@ switch keyword
     case 'Evaluation_Detection_Centered'
         net_dir = [pwd filesep 'Evaluation_Detection_Centered.dir'];
         Nlatent = 32;
-        params_file_name = 'params_2026_05_31__11_47_42.mat';
+        params_file_name = 'params_2026_06_01__10_11_03.mat';
 
 end
 addpath(net_dir);
 cd(net_dir)
 params= load([net_dir filesep params_file_name]);
+cd(mydir)
 
 net_autoencoder = dlnetwork;
 net_decoder =dlnetwork;
@@ -34,15 +35,14 @@ switch keyword
             networkLayer(params.TopLevelModule_decoder.Network,"Name",'TopLevelModule:decoder',"OutputNames",{'6'})];
 
     case 'Evaluation_Detection_Centered'
-        %tempNet = [
-         %   imageInputLayer([121 104 1],"Name","imageinput_1","Normalization","none")
-         %   networkLayer(params.TopLevelModule_encoder.Network,"Name",'TopLevelModule:encoder',"OutputNames",{'11'})
-          %  params.TopLevelModule_ATEN5.Layer
-         %   networkLayer(params.TopLevelModule_to_latent.Network,"Name",'TopLevelModule:to_latent',"OutputNames",{'2'})];
-
-         tempNet = [
-             inputLayer([1 1 120 104],"UUUU","Name","input_1")
-    params.TopLevelModule.Layer];
+        tempNet = [
+            imageInputLayer([120 104 1],"Name","imageinput_1","Normalization","rescale-zero-one")
+            networkLayer(params.TopLevelModule_encoder.Network,"Name",'TopLevelModule:encoder',"OutputNames",{'11'})
+            nnet.pytorch.layer.Flatten2DLayer("TopLevelModule:Flatten0")
+            networkLayer(params.TopLevelModule_to_latent.Network,"Name",'TopLevelModule:to_latent',"OutputNames",{'2'})
+            networkLayer(params.TopLevelModule_from_latent.Network,"Name",'TopLevelModule:from_latent',"OutputNames",{'3'})
+            params.TopLevelModule_ATEN3.Layer
+            networkLayer(params.TopLevelModule_decoder.Network,"Name",'TopLevelModule:decoder',"OutputNames",{'6'})];
 end
 
 
@@ -54,25 +54,45 @@ clear tempNet;
 %%%Finish by initializing the network
 net_autoencoder = initialize(net_autoencoder);
 
-net_decoder=[];
-switch keyword
-    case 'Original_Manual_NotCentered'
-        %%%Create 32-element decoder network
-        tempNet = [
-            inputLayer([Nlatent 1 1 1],"SSCB","Name","input") %The 'C' is channel and 'B' stands for batch and is needed for bulk processing
-            %networkLayer(params.TopLevelModule_encoder.Network,"Name",'TopLevelModule:encoder',"OutputNames",{'11'})
-            %networkLayer(params.TopLevelModule_flatten.Network,"Name",'TopLevelModule:flatten',"OutputNames",{'Flatten0'})
-            %networkLayer(params.TopLevelModule_to_latent.Network,"Name",'TopLevelModule:to_latent',"OutputNames",{'2'})
-            networkLayer(params.TopLevelModule_from_latent.Network,"Name",'TopLevelModule:from_latent',"OutputNames",{'3'})
-            networkLayer(params.TopLevelModule_reshape.Network,"Name",'TopLevelModule:reshape',"OutputNames",{'ATEN2'})
-            networkLayer(params.TopLevelModule_decoder.Network,"Name",'TopLevelModule:decoder',"OutputNames",{'6'})];
-        net_decoder = addLayers(net_decoder,tempNet);
 
-        % clean up helper variable
-        clear tempNet;
+%net_decoder=[];
 
-        net_decoder = initialize(net_decoder);
+try
+    switch keyword
+        case 'Original_Manual_NotCentered'
+            %%%Create 32-element decoder network
+            tempNet = [
+                inputLayer([Nlatent 1 1 1],"SSCB","Name","input") %The 'C' is channel and 'B' stands for batch and is needed for bulk processing
+                %networkLayer(params.TopLevelModule_encoder.Network,"Name",'TopLevelModule:encoder',"OutputNames",{'11'})
+                %networkLayer(params.TopLevelModule_flatten.Network,"Name",'TopLevelModule:flatten',"OutputNames",{'Flatten0'})
+                %networkLayer(params.TopLevelModule_to_latent.Network,"Name",'TopLevelModule:to_latent',"OutputNames",{'2'})
+                networkLayer(params.TopLevelModule_from_latent.Network,"Name",'TopLevelModule:from_latent',"OutputNames",{'3'})
+                networkLayer(params.TopLevelModule_reshape.Network,"Name",'TopLevelModule:reshape',"OutputNames",{'ATEN2'})
+                networkLayer(params.TopLevelModule_decoder.Network,"Name",'TopLevelModule:decoder',"OutputNames",{'6'})];
 
+        case 'Evaluation_Detection_Centered'
+
+            tempNet = [
+                %inputLayer([Nlatent 1 1 1],"SSCB","Name","input") %The 'C' is channel and 'B' stands for batch and is needed for bulk processing
+inputLayer([Nlatent 1 1],"SCB","Name","input") %The 'C' is channel and 'B' stands for batch and is needed for bulk processing
+
+                % imageInputLayer([120 104 1],"Name","imageinput_1","Normalization","rescale-zero-one")
+                %networkLayer(params.TopLevelModule_encoder.Network,"Name",'TopLevelModule:encoder',"OutputNames",{'11'})
+                %nnet.pytorch.layer.Flatten2DLayer("TopLevelModule:Flatten0")
+                %networkLayer(params.TopLevelModule_to_latent.Network,"Name",'TopLevelModule:to_latent',"OutputNames",{'2'})
+                networkLayer(params.TopLevelModule_from_latent.Network,"Name",'TopLevelModule:from_latent',"OutputNames",{'3'})
+                params.TopLevelModule_ATEN3.Layer
+                networkLayer(params.TopLevelModule_decoder.Network,"Name",'TopLevelModule:decoder',"OutputNames",{'6'})];
+
+
+    end
+    net_decoder = addLayers(net_decoder,tempNet);
+
+    % clean up helper variable
+    clear tempNet;
+
+    net_decoder = initialize(net_decoder);
+
+catch
+    disp('Decoder failed')
 end
-
-cd(mydir)
---
+++ matlab/NeuralNetworkImports/master_evaluate_autoencoder_performance.m
@@ -0,0 +1,90 @@
+%%%%master_evaluate_autoencoder_performance.m
+
+close all
+clear all
+
+%%%Load data set used to train autoencoder
+train_fname='/Users/thode/Projects/Greeneridge_bowhead_detection/DeepLearningNPRB_Project/Software/matlab/Displaying_Latent_Spaces';
+train_fname=[train_fname filesep 'latent_embeddings_3d_auto_MATLAB.mat'];
+train=load(train_fname);
+
+
+%%%Load test data set latent vectors
+
+test_dir='/Users/thode/Projects/Greeneridge_bowhead_detection/DeepLearningNPRB_Project/Software/matlab/NeuralNetworkImports';
+test_fname=[test_dir filesep 'Evaluation_Detection_Centered_latent_embeddings_3d_auto_MATLAB.mat'];
+test=load(test_fname);
+
+disp('Computing nearest neighbor....')
+
+Neighbors=20;  %Found no effect in using 10 or 40.
+
+Idx_all=knnsearch(train.latent_embeddings, ...
+    test.latent_embeddings,'K',Neighbors,'IncludeTies',true,'Distance','euclidean');
+Nsamples=length(Idx_all);
+
+threshold=unique([ 0 0:1/Neighbors:1 1]);
+strrr='kr';strr2='bg';
+figure;
+for I=1:2  %1 is original manual labels, 2 is latest relabel
+    train.iscall=train.features.type_org>0 & train.features.type_org<12;
+    test.iscall=test.features.type_org>0 & test.features.type_org<12;
+    switch I
+        case 1
+           Igood=1:Nsamples;
+           Idx=Idx_all;
+           titstr=sprintf('Neighbors: %i, All samples',Neighbors);
+        case 2
+           Igood=find(test.features.duration1>=0.5);
+           Idx=Idx_all(Igood);
+            titstr=sprintf('Neighbors: %i, Samples > 0.5 sec',Neighbors);
+    end
+
+    score=zeros(length(test.iscall(Igood)),1);  %Score is size of test dataset
+    idx_max=0;
+    for Iscore=1:length(Idx)
+        clusster=train.iscall(Idx{Iscore});  %ID (call or not) of nearest neighbors in training set
+        score(Iscore)=sum(clusster)/Neighbors;  %%%Fraction of neigbors that are calls.
+        idx_max=max([idx_max Idx{Iscore}]);
+    end
+    idx_max
+    Icall=test.iscall(Igood)>0;
+    Ino_call=(test.iscall(Igood)==0);
+
+    total_positives_in_test=sum(Icall);
+
+    recall=zeros(1,length(threshold));precision=recall;
+    for Ithresh=1:length(threshold)
+        detected_positives=sum(score(Icall)>=threshold(Ithresh));
+        false_positives=sum(score(Ino_call)>=threshold(Ithresh));
+        recall(Ithresh)=detected_positives./total_positives_in_test;
+        precision(Ithresh)=detected_positives./(detected_positives+false_positives);
+
+    end
+
+
+    subplot(1,2,1);hold on
+    plot(recall,precision,[strrr(I) '-o']);grid on;hold on
+    xlabel('Recall');ylabel('Precision');
+    xlim([0 1]);ylim([0 1]);
+    title(titstr)
+    subplot(1,2,2);hold on
+    plot(1-recall,1-precision,[strrr(I) '-o']);grid on;
+    xlabel('Miss fraction');ylabel('False discovery rate (Fraction of calls that are not calls)');
+    xlim([0 1]);ylim([0 1]);
+    title(titstr)
+    %%%Estimate precision for realistic data set
+    % R1=sum(Ino_call)/sum(Icall);
+    % R2=7.8;  %Ratio of false hits to manual count in full dataset
+    % precision_estimated=precision.*R1./(R2.*(1-precision)+R1.*precision);
+    % subplot(1,2,1)
+    % plot(recall,precision_estimated,[strr2(I) '-o']);
+    % subplot(1,2,2);
+    % plot(1-recall,1-precision_estimated,[strr2(I) '-o']);grid on;hold on
+
+disp(sprintf('Dataset length: %i samples (%i calls, %i false)',length(test.iscall(Igood)),sum(Icall),sum(Ino_call)))
+    
+end %I
+ legend('original','duration restricted');
+ 
+               
